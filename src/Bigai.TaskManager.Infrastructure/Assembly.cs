18f61763fef8e984a747cc27980f96f65f8b3629
Marcos Cruz <marcoscruz@terra.com.br>
2024-11-13 15:08:42 -0300

    refactor: replace mock database by in memory database

--- src/Bigai.TaskManager.Infrastructure/Assembly.cs
+++ src/Bigai.TaskManager.Infrastructure/Assembly.cs
@@ -2,3 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Bigai.TaskManager.Infrastructure.Tests")]
 [assembly: InternalsVisibleTo("Bigai.TaskManager.Application.Tests")]
+[assembly: InternalsVisibleTo("Bigai.TaskManager.Api.Tests")]
--- tests/Bigai.TaskManager.Api.Tests/Controllers/WorkUnitsControllerTests.cs
+++ tests/Bigai.TaskManager.Api.Tests/Controllers/WorkUnitsControllerTests.cs
@@ -1,58 +1,116 @@
 using System.Net;
 
 using Bigai.TaskManager.Api.Tests.Helpers;
-using Bigai.TaskManager.Application.Projects.Commands.CreateProject;
+using Bigai.TaskManager.Application.Projects.Commands.CreateWorkUnit;
 using Bigai.TaskManager.Domain.Projects.Enums;
 using Bigai.TaskManager.Domain.Projects.Models;
 using Bigai.TaskManager.Domain.Projects.Repositories;
 using Bigai.TaskManager.Domain.Projects.Services;
-using Bigai.TaskManager.Domain.Tests.Helpers;
+using Bigai.TaskManager.Infrastructure.Persistence;
+using Bigai.TaskManager.Infrastructure.Projects.Repositories;
+using Bigai.TaskManager.Infrastructure.Projects.Services;
 
 using FluentAssertions;
 
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-
-using Moq;
 
 namespace Bigai.TaskManager.Api.Tests.Controllers;
 
 public class WorkUnitsControllerTests : IClassFixture<WebApplicationFactory<Program>>
 {
     private readonly WebApplicationFactory<Program> _factory;
-    private readonly Mock<IProjectRepository> _projectsRepositoryMock = new();
-    private readonly Mock<IProjectAuthorizationService> _projectAuthorizationServiceMock = new();
+    private readonly IProjectRepository _projectsRepositoryMock;
+    private readonly IProjectAuthorizationService _projectAuthorizationServiceMock;
+    private readonly int _numberOfProjects = 7;
+    private readonly int _userId = 101;
+    private readonly int _numberOfTasks = 7;
 
     public WorkUnitsControllerTests(WebApplicationFactory<Program> factory)
     {
+        var dbContext = GetInMemoryDbContext(_numberOfProjects, _userId, _numberOfTasks);
+        _projectsRepositoryMock = new ProjectRepository(dbContext);
+        _projectAuthorizationServiceMock = new ProjectAuthorizationService();
+
         _factory = factory.WithWebHostBuilder(builder =>
         {
             builder.ConfigureTestServices(services =>
             {
-                services.Replace(ServiceDescriptor.Scoped(typeof(IProjectRepository), _ => _projectsRepositoryMock.Object));
-                services.Replace(ServiceDescriptor.Scoped(typeof(IProjectAuthorizationService), _ => _projectAuthorizationServiceMock.Object));
+                services.Replace(ServiceDescriptor.Scoped(typeof(IProjectRepository), _ => _projectsRepositoryMock));
+                services.Replace(ServiceDescriptor.Scoped(typeof(IProjectAuthorizationService), _ => _projectAuthorizationServiceMock));
             });
         });
     }
+
+    private static TaskManagerDbContext GetInMemoryDbContext(int numberOfProjects, int userId, int numberOfTasks)
+    {
+        var options = new DbContextOptionsBuilder<TaskManagerDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        var context = new TaskManagerDbContext(options);
+
+        List<Project> projects = GetMockedValues(numberOfProjects, userId, numberOfTasks);
+
+        if (projects is not null && projects.Count > 0)
+        {
+            context.Projects.AddRange(projects);
+
+            context.SaveChanges();
+        }
+
+        return context;
+    }
+
+    private static List<Project> GetMockedValues(int numberOfProjects, int userId, int numberOfTasks)
+    {
+        var projects = new List<Project>();
+
+        if (numberOfProjects > 0)
+        {
+            Random rnd = new();
+
+            for (int i = 0; i < numberOfProjects; i++)
+            {
+                var project = Project.Create($"Project name {Guid.NewGuid()}");
+
+                for (int j = 0; j < numberOfTasks; j++)
+                {
+                    var dueDate = DateTimeOffset.Now.AddDays(rnd.Next(15, 45));
+                    var priority = (Priority)rnd.Next(0, 2);
+                    var workUnit = WorkUnit.Create("Title of task", "Description of task", dueDate, priority);
+
+                    if (i == 0)
+                    {
+                        workUnit.ChangeStatus(Status.Completed);
+                    }
+
+                    workUnit.AssignToUser(userId);
+                    project.AddWorkUnit(workUnit);
+                }
+
+                projects.Add(project);
+            }
+        }
+
+        return projects;
+    }
+
 
     [Fact]
     public async Task GetWorkUnitsByProjectIdAsync_ReturnsStatus200OK()
     {
         // arrange
-        int registeredUser = 1001;
-        int projectId = 1;
-        int amountProjects = 15;
-        IReadOnlyCollection<Project> projects = ProjectHelper.GetProjects(amountProjects, registeredUser);
-
-        _projectsRepositoryMock.Setup(p => p.GetProjectByIdAsync(projectId, CancellationToken.None))
-                               .ReturnsAsync(projects.First());
+        var projects = await _projectsRepositoryMock.GetProjectsByUserIdAsync(_userId);
+        var project = projects.ToArray()[0];
 
         var client = _factory.CreateClient();
 
         // act
-        var response = await client.GetAsync($"/api/projects/{projectId}/tasks/");
+        var response = await client.GetAsync($"/api/projects/{project.Id}/tasks/");
 
         // assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
@@ -62,11 +120,7 @@ public class WorkUnitsControllerTests : IClassFixture<WebApplicationFactory<Prog
     public async Task GetWorkUnitsByProjectIdAsync_ReturnsStatus404NotFound()
     {
         // arrange
-        int projectId = 1;
-        Project? project = null;
-
-        _projectsRepositoryMock.Setup(p => p.GetProjectByIdAsync(projectId, CancellationToken.None))
-                               .ReturnsAsync(project);
+        int projectId = 101001;
 
         var client = _factory.CreateClient();
 
@@ -81,15 +135,9 @@ public class WorkUnitsControllerTests : IClassFixture<WebApplicationFactory<Prog
     public async Task GetUnitWorkByIdAsync_ReturnsStatus200OK()
     {
         // arrange
-        int registeredUser = 1001;
-        int amountProjects = 15;
-        IReadOnlyCollection<Project> projects = ProjectHelper.GetProjects(amountProjects, registeredUser);
-        Project project = projects.First();
-        WorkUnit workUnit = project.WorkUnits.ToArray()[0];
-
-        _projectsRepositoryMock.Setup(p => p.GetWorkUnitByIdAsync(workUnit.Id, CancellationToken.None))
-                               .ReturnsAsync(workUnit);
-
+        var projects = await _projectsRepositoryMock.GetProjectsByUserIdAsync(_userId);
+        var project = projects.ToArray()[0];
+        var workUnit = project.WorkUnits.ToArray()[0];
         var client = _factory.CreateClient();
 
         // act
@@ -103,23 +151,55 @@ public class WorkUnitsControllerTests : IClassFixture<WebApplicationFactory<Prog
     public async Task GetProjectByIdAsync_ReturnsStatus404NotFound()
     {
         // arrange
-        int registeredUser = 1001;
-        int amountProjects = 15;
-        int workUnitId = 77;
-        IReadOnlyCollection<Project> projects = ProjectHelper.GetProjects(amountProjects, registeredUser);
-        Project project = projects.First();
-        WorkUnit? workUnit = null;
-
-        _projectsRepositoryMock.Setup(p => p.GetWorkUnitByIdAsync(workUnitId, CancellationToken.None))
-                               .ReturnsAsync(workUnit);
+        var projectId = 101001;
+        var workUnitId = 12;
 
         var client = _factory.CreateClient();
 
         // act
-        var response = await client.GetAsync($"/api/projects/{project.Id}/tasks/{workUnitId}");
+        var response = await client.GetAsync($"/api/projects/{projectId}/tasks/{workUnitId}");
 
         // assert
         response.StatusCode.Should().Be(HttpStatusCode.NotFound);
     }
 
+    [Fact]
+    public async Task CreateAsync_ReturnsStatus201Created()
+    {
+        // arrange
+        var projects = await _projectsRepositoryMock.GetProjectsByUserIdAsync(_userId);
+        var project = projects.ToArray()[2];
+
+        var command = new CreateWorkUnitCommand
+        {
+            ProjectId = project.Id,
+            Title = "Title of new work unit test",
+            Description = "Description of new work unit test",
+            DueDate = DateTime.Now.AddDays(15),
+            Priority = Priority.Average
+        };
+
+        var client = _factory.CreateClient();
+
+        // act
+        var response = await client.PostAsync("/api/projects", TestHelper.GetJsonStringContent(command));
+
+        // assert
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+    }
+
+    [Fact]
+    public async Task CreateAsync_ReturnsStatus400BadRequest()
+    {
+        // arrange
+        var command = new CreateWorkUnitCommand();
+
+        var client = _factory.CreateClient();
+
+        // act
+        var response = await client.PostAsync("/api/projects", TestHelper.GetJsonStringContent(command));
+
+        // assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
 }
